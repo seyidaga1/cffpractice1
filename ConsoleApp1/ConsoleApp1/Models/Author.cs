d2c8f06ada9e4099e3b2389ecff18b7d085af074
Xalidaa <hacizadexalide@gmail.com>
2025-12-01 20:23:02 +0400

    inheritence added

--- ConsoleApp1/ConsoleApp1/Models/Author.cs
+++ ConsoleApp1/ConsoleApp1/Models/Author.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.Models
 {
-    internal class Author
+    internal class Author :Book
     {
         public string Name { get; set; }
         public string Surname { get; set; }
         public int NumberOfBooks { get; set; }
+
+        public void DisplayAuthorInfo()
+        {
+            Console.WriteLine($"Author: {Name} {Surname}, Number of Books: {NumberOfBooks}, Title: {Title}");
+        }
     }
 }
