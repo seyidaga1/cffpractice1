893b0a27bc61e05c4186d3be0823c31594c6de07
seyidaga1 <seyidaga221@gmail.com>
2025-12-01 20:15:33 +0400

    'add'

---
+++ ConsoleApp1/ConsoleApp1/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class Author
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int NumberOfBooks { get; set; }
+    }
+}
